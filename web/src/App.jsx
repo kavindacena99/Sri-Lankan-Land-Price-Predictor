0fde0c93175a66781f268298d7ebae5a14c3828c
kavinda <kavindach4@gmail.com>
2025-04-16 23:33:19 +0530

    Add Bootstrap for styling and enhance form layout in land price predictor

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 function App(){
   const [location,setLocation] = useState("");
-  const [size,setSize] = useState(0.0);
+  const [size,setSize] = useState();
   const [price,setPrice] = useState(0.00);
 
   const predictPrice = async () => {
@@ -23,15 +23,19 @@ function App(){
 
   return(
     <div>
-      <h1>Sri Lanka Land Price Predictor</h1>
+      <h1 className='text-center'>Sri Lanka Land Price Predictor</h1>
 
-      <form onSubmit={(e) => { e.preventDefault(); predictPrice();}}>
-        <input type="text" placeholder='Location Ex:Colombo ' value={location} onChange={(e)=>setLocation(e.target.value)} required />
-        <input type="number" placeholder='Size in Perches' value={size} onChange={(e)=>setSize(e.target.value)} required />
-        <button type="submit">Predict Price</button>
-      </form>
+      <div className='row'>
+          <form onSubmit={(e) => { e.preventDefault(); predictPrice();}} style={{ margin:'20px', width:'650px',border:'1px solid black',padding:'20px',borderRadius:'10px'}}>
+            <label htmlFor="location">District:</label> <br />
+            <input type="text" placeholder='Input a District' value={location} onChange={(e)=>setLocation(e.target.value)} required className='form-control' /> <br />
+            <label htmlFor="size">Size:</label>
+            <input type="number" placeholder='Size in Perches' value={size} onChange={(e)=>setSize(e.target.value)} required className='form-control' /> <br />
+            <button type="submit" className='btn btn-primary'>Predict Price</button>
+          </form>
 
-      {price && <h2>Predicted Price: {price} LKR</h2>}
+          <h2>Predicted Price: {price} LKR</h2>
+      </div>
     </div>
   );
 }
