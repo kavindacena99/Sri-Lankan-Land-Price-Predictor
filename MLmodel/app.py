aea378d263bee8f50b74f1cdfb929247476bc1bc
kavinda <kavindach4@gmail.com>
2025-04-16 20:36:57 +0530

    python API created

---
+++ MLmodel/app.py
@@ -0,0 +1,27 @@
+from flask import Flask, request, jsonify
+import joblib
+import numpy as np
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+model = joblib.load('land_price_model.pkl')
+encoder = joblib.load('location_encoder.pkl')
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    data = request.json
+    location = data["location"]
+    size = data["size"]
+
+    try:
+        encoded_location = encoder.transform([location])[0]
+        prediction = model.predict([[encoded_location, size]])[0]
+        return jsonify({"predicted_price": round(prediction,2)})
+    
+    except:
+        return jsonify({"error": "Invalid location or size"}), 400
+    
+if __name__ == '__main__':
+    app.run(debug=True)
